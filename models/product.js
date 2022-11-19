536f96c2617da627cd1ea212fae7cddd42db394d
acruz101 <47541514+acruz101@users.noreply.github.com>
2022-11-18 16:43:19 -0800

    initial commit

---
+++ models/product.js
@@ -0,0 +1,24 @@
+// creating our model
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    category: {
+        type: String,
+        lowercase: true,
+        enum: ['fruit', 'vegetable', 'dairy']
+    }
+})
+
+const Product = mongoose.model('Product', productSchema);
+
+// export model for use somewhere else
+module.exports = Product;
