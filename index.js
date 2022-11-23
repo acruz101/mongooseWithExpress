95ef108b02fda65184dfc4f68b037f4f234d0de2
acruz101 <47541514+acruz101@users.noreply.github.com>
2022-11-22 17:06:53 -0800

    add error handling with express - basic

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-// set up
 const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+
+const methodOverride = require('method-override');
+
+const AppError = require('./AppError');
 const Product = require('./models/product'); // creating our model
-const methodOverride = require('method-override')
 
 main()
     .then(() => console.log('MONGO CONNECTION OPEN'))
@@ -16,62 +18,87 @@ async function main() {
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }))
+
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
 
-const categories = ['fruit', 'vegetable', 'dairy']
+const categories = ['fruit', 'vegetable', 'dairy'];
 
-app.get('/products', async (req, res) => {
+app.get('/products', wrapAsync(async (req, res, next) => {
     const { category } = req.query;
     if (category) {
-        const products = await Product.find({ category });
-        res.render('products/index', { products, category });
+        const products = await Product.find({ category })
+        res.render('products/index', { products, category })
     } else {
-        const products = await Product.find({}); // async/await it bc it takes time
-        res.render('products/index', { products, category: 'All' });;
+        const products = await Product.find({})
+        res.render('products/index', { products, category: 'All' })
     }
+}))
 
+app.get('/products/new', (req, res) => {
+    // throw new AppError('Not allowed', 401);
+    res.render('products/new', { categories })
 })
 
-app.get('/products/new', async (req, res) => {
-    res.render('products/new', { categories });
-})
-app.post('/products', async (req, res) => {
+app.post('/products', wrapAsync(async (req, res, next) => {
     const newProduct = new Product(req.body);
     await newProduct.save();
-    res.redirect(`/products/${newProduct._id}`)
-})
+    res.redirect(`/products/${newProduct._id}`);
+}))
 
-// product details
-app.get('/products/:id', async (req, res) => {
+function wrapAsync(fn) {
+    return function (req, res, next) {
+        fn(req, res, next).catch(e => next(e));
+    }
+}
+
+app.get('/products/:id', wrapAsync(async (req, res, next) => {
     const { id } = req.params;
-    const product = await Product.findById(id);
-    console.log(product);
+    const product = await Product.findById(id)
+    if (!product) {
+        throw new AppError('Product not found', 404);
+    }
     res.render('products/show', { product });
-})
+}))
 
-//update product
-app.get('/products/:id/edit', async (req, res) => {
+app.get('/products/:id/edit', wrapAsync(async (req, res, next) => {
     const { id } = req.params;
     const product = await Product.findById(id);
-    res.render('products/edit', { product, categories });
-})
-app.put('/products/:id', async (req, res) => {
+    if (!product) {
+        throw new AppError('Product not found', 404);
+    }
+    res.render('products/edit', { product, categories })
+}));
+
+app.put('/products/:id', wrapAsync(async (req, res, next) => {
     const { id } = req.params;
     const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
     res.redirect(`/products/${product._id}`);
-})
+}))
 
-//delete product
-app.delete('/products/:id', async (req, res) => {
+app.delete('/products/:id', wrapAsync(async (req, res) => {
     const { id } = req.params;
     const deletedProduct = await Product.findByIdAndDelete(id);
     res.redirect('/products');
+}))
+
+const handleValidationErr = err => {
+    console.dir(err);
+    return new AppError(`Validation Failed...${err.message}`, 400);
+}
+app.use((err, req, res, next) => {
+    console.log(err.name);
+    if(err.name === 'ValidationError') {
+        err = handleValidationErr(err)
+    }
+    next(err);
 })
 
+app.use((err, req, res, next) => {
+    const { status = 500, message = 'Something went wrong' } = err;
+    res.status(status).send(message);
+})
 
 app.listen(3000, () => {
-    console.log('listening on port 3000');
+    console.log("APP IS LISTENING ON PORT 3000!")
 })
-
-// end set up
